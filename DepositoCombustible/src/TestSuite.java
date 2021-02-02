22c32432de02515539d6e1416c9266547539e6ef
Juan Alberto <juan.selfaleal@gmail.com>
2021-02-02 18:34:32 +0100

    Incorporación de todas las pruebas a la Suite.

---
+++ DepositoCombustible/src/TestSuite.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ prueba1.class, prueba2.class, prueba3.class, prueba4.class, prueba5.class, prueba6.class })
+public class TestSuite {
+
+}
